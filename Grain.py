c3f90638626ecbed41eccf26bdf5966aed932ba8
MrGribel <joao.gribel@hotmail.com>
2022-02-14 16:00:21 -0400

    ENH: refactoring and variable name change

--- Grain.py
+++ Grain.py
@@ -12,23 +12,23 @@ class Grain:
         self,
         outer_radius,
         initial_inner_radius,
-        geometry="tubular",
         initial_height=None,
+        geometry="tubular"
     ):
 
         self.outer_radius = outer_radius
-        self.initial_inner_radius = initial_inner_radius
+        self.inner_radius = initial_inner_radius
         self.evaluate_grain_height(initial_height)
         self.geometry = geometry
         self.evaluate_grain_geometry()
 
-    def evaluate_grain_height(self, initial_height):
-        if initial_height is None:
-            self.initial_height = 3 * self.outer_radius + self.initial_inner_radius  ### Verificar
+    def evaluate_grain_height(self, height):
+        if height is None:
+            self.height = 3 * self.outer_radius + self.initial_inner_radius  ### Verificar
         else:
-            self.initial_height = initial_height
+            self.height = height
 
-        return self.initial_height
+        return self.height
 
     def evaluate_grain_geometry(self):
         if self.geometry == "tubular":
@@ -39,7 +39,8 @@ class Grain:
             print("Not a valid geometry type")
 
     def evaluate_tubular_grain_area(self):
-        transversal_area = 2 * np.pi * (self.outer_radius ** 2 - self.initial_inner_radius ** 2)
-        longitudinal_area = 2 * np.pi * self.initial_inner_radius * self.initial_height
+        transversal_area = 2 * np.pi * (self.outer_radius ** 2 - self.inner_radius ** 2)
+        longitudinal_area = 2 * np.pi * self.inner_radius * self.height
         self.grain_area = transversal_area + longitudinal_area
+        
         return self.grain_area
