8162a54553eff291e172160faaecae2a18b4a959
phmb <phmbressan@usp.br>
2022-02-21 10:02:30 -0300

    Inexact Simulation of Outside for debugging. Added support for csv thrust input from Static Fire data.

--- solidpy/Outside.py
+++ solidpy/Outside.py
@@ -5,7 +5,9 @@ _copyright_ = "x"
 _license_ = "x"
 
 
+import csv
 import numpy as np
+from scipy import interpolate
 from scipy.integrate import solve_ivp
 
 
@@ -34,26 +36,29 @@ class Outside:
         self.air_density = air_density
         self.rail_length = rail_length
         self.rail_angle = rail_angle  # wrt. horizontal
-        self.evaluate_frontal_area(self, frontal_area)
+        self.evaluate_frontal_area(frontal_area)
 
         # Thrust vector temporary state
         # pending future "Burn" class
         self.thrust = thrust
 
-        # Initial state variable conditions
+        # Initial differential equation conditions
         self.initial_position = 0.0
         self.initial_velocity = 0.0
+        self.time_span = 29.086, 34.36
+        # M for Manual, A for Auto, ESn for Evenly Spaced (n=number steps)
+        self.time_method = "M"
 
         # Differential equation solution
         self.end_rail_velocity = self.solve_rail()
 
     def evaluate_frontal_area(self, frontal_area):
         if frontal_area is None:
-            frontal_area = np.pi * (self.rocket_radius**2)
+            self.frontal_area = np.pi * (self.rocket_radius**2)
         else:
             self.frontal_area = frontal_area
 
-    def vector_field(self, state_variables, time, parameters):
+    def vector_field(self, time, state_variables, parameters):
         position, velocity = state_variables
         (
             gravity,
@@ -68,14 +73,29 @@ class Outside:
 
         vector_state = [
             velocity,
-            (thrust - k_drag * velocity**2) / (rocket_mass)
+            (thrust(time) - k_drag * velocity**2) / (rocket_mass)
             - gravity * np.sin(rail_angle),
         ]
 
         return vector_state
 
+    def set_time_steps(self):
+        time_method = self.time_method
+        time_span = self.time_span
+        if time_method == "M":
+            return ext_time_list
+        elif time_method[:2] == "ES":
+            number_steps = int(time_method[2:])
+            time_steps = np.linspace(time_span[0], time_span[1], number_steps)
+            return time_steps
+        return None
+
     def solve_rail(self):
+
+        """Variables"""
         state_variables = [self.initial_position, self.initial_velocity]
+
+        """Parameters"""
         parameters = [
             self.gravity,
             self.rocket_mass,
@@ -85,27 +105,46 @@ class Outside:
             self.rail_angle,
             self.thrust,
         ]
-        time_span = 0.0, 1.0  # initial,  final
         absolute_error = 1.0e-12
         relative_error = 1.0e-8
 
+        """Solver"""
+        # uncomment "atol" and "rtol" for manual error control
         solution = solve_ivp(
             self.vector_field,
-            time_span,
+            self.time_span,
             state_variables,
             method="RK45",
+            t_eval=self.set_time_steps(),
             args=(parameters,),
-            # atol=absolute_error, uncomment these lines
-            # rtol=relative_error, for manual error control
+            # atol=absolute_error,
+            # rtol=relative_error,
         )
 
-        with open("rail_data.dat", "w") as rail_data:
-            for solution_var, solution_time in zip(solution.y, solution.t):
-                if solution_var[0] > self.rail_length:
+        """Export data"""
+        with open("data/rail_movement/rail_data.csv", "w") as rail_data:
+            solution_writer = csv.writer(rail_data)
+            solution_writer.writerow(["Time", "Position", "Velocity"])
+            for solution_time, solution_position, solution_velocity in zip(
+                solution.t, solution.y[0], solution.y[1]
+            ):
+                if solution_position > self.rail_length:
                     break
                 else:
-                    print(
-                        solution_time, solution_var[0], solution_var[1], file=rail_data
+                    solution_writer.writerow(
+                        [solution_time, solution_position, solution_velocity]
                     )
 
-        return solution_var[1]
+        return solution_velocity
+
+
+"""Simulation for testing and debugging purposes only"""
+
+data_path = "data/static_fires/Keron.csv"
+ext_time_list, ext_thrust = np.loadtxt(
+    data_path, delimiter=";", unpack=True, skiprows=1
+)
+thrust = interpolate.interp1d(ext_time_list, ext_thrust)
+
+Keron_test = Outside(10e5, 10, 21.4e-3, 0.5, 1.25, 10, np.pi / 2, None, thrust)
+print(Keron_test.end_rail_velocity)
