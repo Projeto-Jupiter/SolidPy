2e529d5010a03e7f6b60646c0e22f0c2e5c9e8c7
Pedro Bressan <phmbressan@usp.br>
2022-09-23 08:39:31 -0300

    maint: generate setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="SolidPy",
+    install_requires=[
+        "ambiance>=1.3.0",
+        "matplotlib>=3.5.1",
+        "numpy>=1.19.5",
+        "scikit_fmm>=2022.8.15",
+        "scipy>=1.8.0"
+    ],
+)
